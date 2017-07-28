bf290e1f9ab7329f07dc8f5c736500139f58c2d7
Leonard Weigand <leo@weigand.io>
2017-07-28 22:50:40 +0200

    switch to local electron, add live reload
    This commit removes the global electron dependency and uses a locally installed one instead. This makes the installation process easier and prevents version conflicts.
    It also adds the `electron-reload` package for livereload-like refreshing of Browser Windows.

--- window.js
+++ window.js
@@ -1,12 +1,16 @@
 const electron = require('electron')
-require('electron-reload')(__dirname);
+const path = require('path')
+const url = require('url')
+
+if(process.argv[2] == "--watch"){
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
+}
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-
-const path = require('path')
-const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
